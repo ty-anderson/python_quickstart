c9c8eba8a1a1c8e32765214a95a3767f82b1cd0f
Tyler Anderson <tyleranderson827@gmail.com>
2025-06-27 01:20:49 -0600

    update

--- save_and_deploy.sh
+++ save_and_deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 mkdocs build
+.venv/bin/python to_pdf.py
 #mkdocs gh-deploy
 git add .
 git commit -m "update"
 git push
 rsync -av ./site/ tyler@192.168.1.104:/srv/web_apps/notes/
-.venv/bin/python to_pdf.py
