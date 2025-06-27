89d4b0a9febb2d05e68cd3cca50fedade92d03bf
Tyler Anderson <tyleranderson827@gmail.com>
2025-06-27 01:18:07 -0600

    update

--- save_and_deploy.sh
+++ save_and_deploy.sh
@@ -6,5 +6,4 @@ git add .
 git commit -m "update"
 git push
 rsync -av ./site/ tyler@192.168.1.104:/srv/web_apps/notes/
-.venv/bin/activate
-source python to_pdf.py
+source .venv/bin/python to_pdf.py
