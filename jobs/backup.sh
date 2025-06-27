fa73b2a7aad0ac27e1babbf28685e790896e61c4
Tyler Anderson <tyleranderson827@gmail.com>
2025-06-27 01:34:09 -0600

    update

--- jobs/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -avz --rsync-path="sudo rsync" /Users/tyleranderson/tmp/ tyler@anderson.home:/home/tyler/.backup/tmp
-echo "Ran on $(date +%Y-%m-%d--%H:%M:%S)" >> logs/backup.log
