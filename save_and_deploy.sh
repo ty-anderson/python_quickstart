f6c15ac227b8c5b36e990bb84ab451fe418a7b3c
Tyler Anderson <tyleranderson827@gmail.com>
2025-07-30 22:51:39 -0600

    update

--- save_and_deploy.sh
+++ save_and_deploy.sh
@@ -6,4 +6,4 @@ mkdocs build
 git add .
 git commit -m "update"
 git push
-rsync -av --rsync-path="sudo rsync" ./site/ tyler@192.168.1.104:/srv/web_apps/notes/
+rsync -av --delete --rsync-path="sudo rsync" ./site/ tyler@192.168.1.104:/srv/web_apps/notes/
