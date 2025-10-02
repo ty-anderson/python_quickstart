66f34376159602806c9164266adf63bc3343f3f7
Tyler Anderson <tyleranderson827@gmail.com>
2025-10-01 18:08:23 -0600

    update

--- save_and_deploy.sh
+++ save_and_deploy.sh
@@ -5,7 +5,13 @@ mkdocs build
 # mkdocs gh-deploy
 git add .
 git commit -m "update"
-git push origin master
+#git push origin master
 git push backup master
 rsync -av --delete --rsync-path="sudo rsync" ./site/ tyler@192.168.1.104:/srv/web_apps/notes/
+
+# public version
+mv mkdocs.yml mkdocs_original.yml
+mv mkdocs_public.yml mkdocs.yml
+mkdocs build
 rsync -av --delete --rsync-path="sudo rsync" ./site/ tyler@192.168.1.104:/srv/web_apps/public_notes/
+mv mkdocs_original.yml mkdocs.yml
